7b0b2cb031ac0db995c599a5c973890428a40842
Adrian Siekierka <kontakt@asie.pl>
2023-09-06 20:08:47 +0200

    extlib/plum: add libplum LICENSE file, fix comments

--- extlib/plum.h
+++ extlib/plum.h
@@ -1,15 +1,10 @@
 //
 //! \file plum.h
 //!  Extra libplum stuff
+//!  (adapted from original FreeImage wrappers)
 //! \date 20070226 - 20070226
 //! \author cearn
-// === NOTES ===
-// * Now, why am I using CLDIB stuff when I could just as 
-//	 well have use FreeImage all round? Well, historical reasons, I 
-//	 guess. The FreeImage library is large, VERY large. And to use 
-//	 a library that's bigger than the app itself seems strange.
-//	 Aside from that, FI didn't quite have all I wanted when I 
-//	 started this, so internally I'm using something that is.
+//! \author asie
 
 #ifndef __FI_PLUM_H__
 #define __FI_PLUM_H__
@@ -18,13 +13,45 @@
 
 #include "libplum.h"
 
+/**
+ * @brief Initialize the CLDIB <-> libplum translation layer.
+ */
 void plumInit();
 
-// For converting between CLDIB and FIBITMAP
-// and saving/loading. 
+/**
+ * @brief Convert a libplum image struct to a CLDIB image struct.
+ * The original libplum image struct is NOT destroyed.
+ * 
+ * @param fi libplum image struct.
+ * @return CLDIB* CLDIB image struct, or NULL if error occured.
+ */
 CLDIB *plum2dib(struct plum_image *fi);
+
+/**
+ * @brief Convert a CLDIB image struct to a libplum image struct.
+ * The original CLDIB image struct is NOT destroyed.
+ * 
+ * @param dib CLDIB image struct.
+ * @return struct plum_image* libplum image struct, or NULL if error occured.
+ */
 struct plum_image *dib2plum(CLDIB *dib);
+
+/**
+ * @brief Load CLDIB image using libplum's image I/O facilities.
+ * 
+ * @param fpath Path to image file.
+ * @return CLDIB* CLDIB image struct, or NULL if error occured.
+ */
 CLDIB *cldib_load(const char *fpath, void *extra);
+
+/**
+ * @brief Save CLDIB image using libplum's image I/O facilities.
+ * 
+ * @param dib CLDIB image struct.
+ * @param fpath Path to image file.
+ * @return true Image saved successfully.
+ * @return false Error occured.
+ */
 bool cldib_save(const CLDIB *dib, const char *fpath, void *extra);
 
 /*!	\}	*/
